bb999c9311094ee41f28ec33d1a75829ffe29b05
J-Andersen-UvA <j.andersen@uva.nl>
2025-11-13 13:20:43 +0100

    initial init

---
+++ Source/LiveLinkMultiIPhone/LiveLinkMultiIPhone.Build.cs
@@ -0,0 +1,11 @@
+﻿using UnrealBuildTool;
+
+public class LiveLinkMultiIPhone : ModuleRules
+{
+    public LiveLinkMultiIPhone(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "Slate", "SlateCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "UnrealEd" });
+    }
+}
